4e67154dbff34e0e14ce1852e2c27b9f90144c1f
chrisjaechun <chrisjaechun@gmail.com>
2021-01-26 11:29:40 -0500

    Update User model and user_routes for username
    - Added a username property for User model and user_routes

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -44,6 +44,7 @@ router.post('/sign-up', (req, res, next) => {
     .then(hash => {
       // return necessary params to create a user
       return {
+        username: req.body.credentials.usename,
         email: req.body.credentials.email,
         hashedPassword: hash
       }
